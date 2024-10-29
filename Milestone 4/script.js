2f80fb12781aa0ec02d77700dd569f9c0569a396
JaweriaMustafa-github <jaweriamustafa01nov@gmail.com>
2024-10-29 11:53:38 +0500

    Added Milestone 4

--- Milestone 4/script.js
+++ Milestone 4/script.js
@@ -2,21 +2,40 @@ var form = document.getElementById('resume-form');
 var resumeDisplayElement = document.getElementById('resume-display');
 // Handle form submission
 form.addEventListener('submit', function (e) {
+    var _a;
     e.preventDefault(); //prevent page reload
     // Collect input values
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
-    var education = document.getElementById('education').value;
-    var experience = document.getElementById('experience').value;
-    var skills = document.getElementById('skills').value;
-    // Generate the resume dynamically
-    var resumeHTML = "\n    <h2>Editable Resume</h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b> <span contenteditable=\"true\"> ".concat(name, "</span></p>\n    <p><b>Email:</b> <span contenteditable=\"true\"> ").concat(email, "</span></p>\n    <p><b>Phone No:</b> <span contenteditable=\"true\"> ").concat(phone, "</span></p>\n\n    <h3>Education</h3>\n    <p contenteditable=\"true\">").concat(education, "</p>\n\n    <h3>Experience</h3>\n    <p contenteditable=\"true\">").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p contenteditable=\"true\">").concat(skills, "</p>\n    ");
-    // Display the generated resume
-    if (resumeDisplayElement) {
-        resumeDisplayElement.innerHTML = resumeHTML;
-    }
-    else {
-        console.error("The resume display element is missing");
+    var profilePictureInput = document.getElementById('profilePicture');
+    var objectiveElement = document.getElementById('objective');
+    var certificationElement = document.getElementById('certification');
+    var nameElement = document.getElementById('name');
+    var emailElement = document.getElementById('email');
+    var phoneElement = document.getElementById('phone');
+    var educationElement = document.getElementById('education');
+    var experienceElement = document.getElementById('experience');
+    var skillsElement = document.getElementById('skills');
+    if (profilePictureInput && objectiveElement && certificationElement && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement) {
+        var profilePicture = profilePictureInput.value;
+        var objective = objectiveElement.value;
+        var certification = certificationElement.value;
+        var name_1 = nameElement.value;
+        var email = emailElement.value;
+        var phone = phoneElement.value;
+        var education = educationElement.value;
+        var experience = experienceElement.value;
+        var skills = skillsElement.value;
+        // Profile Picture
+        var profilePictureFile = (_a = profilePictureInput.files) === null || _a === void 0 ? void 0 : _a[0];
+        var profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : '';
+        // Generate the resume dynamically
+        var resumeHTML = "\n         <h2>Editable Resume</h2>\n\n         <fieldset>\n         <h3>Profile Section:</h3>\n         <p><b>Profile Picture: </b> <span contenteditable=\"true\">".concat(profilePicture ? "<img  src=\"".concat(profilePictureURL, "\" alt=\"Profile Picture\" class=\"profilePicture\">") : "", "</span></p>\n         <section id=\"objective\">\n            <p><b>Objective:</b><span contenteditable=\"true\"> ").concat(objective, "</span><p>\n        </section>\n        <section id=\"certification\">\n            <p><b>Certifications:</b><span contenteditable=\"true\"> ").concat(certification, "</span><p>\n        </section>\n        </fieldset>\n\n        <fieldset >\n        <h3>Personal Information:</h3>\n        <p><b>Name:</b><span contenteditable=\"true\"> ").concat(name_1, "</span></p>\n        <p><b>Email:</b><span contenteditable=\"true\"> ").concat(email, "</span></p>\n        <p><b>Phone No:</b><span contenteditable=\"true\"> ").concat(phone, "</span></p>\n</fieldset>\n\n    <fieldset >\n    <h3>Education:</h3>\n    <p contenteditable=\"true\">").concat(education, "</p>\n</fieldset>\n\n    <fieldset >\n    <h3>Experience:</h3>\n    <p contenteditable=\"true\">").concat(experience, "</p>\n</fieldset>\n\n    <fieldset >\n    <h3>Skills:</h3>\n    <p contenteditable=\"true\">").concat(skills, "</p>\n    </fieldset>\n    ");
+        // Display the generated resume
+        if (resumeDisplayElement) {
+            resumeDisplayElement.innerHTML = resumeHTML;
+            resumeDisplayElement.style.display = 'block';
+        }
+        else {
+            console.error("The resume display element is missing");
+        }
     }
 });
--- Milestone 4/script.ts
+++ Milestone 4/script.ts
@@ -1,42 +1,81 @@
 const form = document.getElementById('resume-form') as HTMLFormElement;
-const resumeDisplayElement = document.getElementById('resume-display') as HTMLDivElement;
+const resumeDisplayElement = document.getElementById('resume-display')as HTMLDivElement;
 
 // Handle form submission
 form.addEventListener('submit', (e:Event) => {
     e.preventDefault(); //prevent page reload
 
     // Collect input values
-    const name = (document.getElementById('name') as HTMLInputElement).value;
-    const email = (document.getElementById('email') as HTMLInputElement).value;
-    const phone = (document.getElementById('phone') as HTMLInputElement).value;
-    const education = (document.getElementById('education') as HTMLInputElement).value;
-    const experience = (document.getElementById('experience') as HTMLInputElement).value;
-    const skills = (document.getElementById('skills') as HTMLInputElement).value;
+    const profilePictureInput = (document.getElementById('profilePicture') as HTMLInputElement);
+    const objectiveElement = (document.getElementById('objective') as HTMLInputElement);
+    const certificationElement = (document.getElementById('certification') as HTMLInputElement);
+    const nameElement = (document.getElementById('name') as HTMLInputElement);
+    const emailElement = (document.getElementById('email') as HTMLInputElement);
+    const phoneElement = (document.getElementById('phone') as HTMLInputElement);
+    const educationElement = (document.getElementById('education') as HTMLTextAreaElement);
+    const experienceElement = (document.getElementById('experience') as HTMLTextAreaElement);
+    const skillsElement = (document.getElementById('skills') as HTMLTextAreaElement);
+
+    if (profilePictureInput && objectiveElement && certificationElement && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement) {
+        const profilePicture = profilePictureInput.value;
+        const objective = objectiveElement.value;
+        const certification = certificationElement.value;
+        const name = nameElement.value;
+        const email = emailElement.value;
+        const phone = phoneElement.value;
+        const education = educationElement.value;
+        const experience = experienceElement.value;
+        const skills = skillsElement.value;
+  
+
+    // Profile Picture
+    const profilePictureFile = profilePictureInput.files?.[0];
+    const profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : '';
 
     // Generate the resume dynamically
     const resumeHTML = `
-    <h2>Editable Resume</h2>
-    <h3>Personal Information</h3>
-    <p><b>Name:</b> <span contenteditable="true"> ${name}</span></p>
-    <p><b>Email:</b> <span contenteditable="true"> ${email}</span></p>
-    <p><b>Phone No:</b> <span contenteditable="true"> ${phone}</span></p>
+         <h2>Editable Resume</h2>
 
-    <h3>Education</h3>
+         <fieldset>
+         <h3>Profile Section:</h3>
+         <p><b>Profile Picture: </b> <span contenteditable="true">${profilePicture ? `<img  src="${profilePictureURL}" alt="Profile Picture" class="profilePicture">` : "" }</span></p>
+         <section id="objective">
+            <p><b>Objective:</b><span contenteditable="true"> ${objective}</span><p>
+        </section>
+        <section id="certification">
+            <p><b>Certifications:</b><span contenteditable="true"> ${certification}</span><p>
+        </section>
+        </fieldset>
+
+        <fieldset >
+        <h3>Personal Information:</h3>
+        <p><b>Name:</b><span contenteditable="true"> ${name}</span></p>
+        <p><b>Email:</b><span contenteditable="true"> ${email}</span></p>
+        <p><b>Phone No:</b><span contenteditable="true"> ${phone}</span></p>
+</fieldset>
+
+    <fieldset >
+    <h3>Education:</h3>
     <p contenteditable="true">${education}</p>
+</fieldset>
 
-    <h3>Experience</h3>
+    <fieldset >
+    <h3>Experience:</h3>
     <p contenteditable="true">${experience}</p>
+</fieldset>
 
-    <h3>Skills</h3>
+    <fieldset >
+    <h3>Skills:</h3>
     <p contenteditable="true">${skills}</p>
+    </fieldset>
     `;
-
     // Display the generated resume
     if(resumeDisplayElement) {
         resumeDisplayElement.innerHTML = resumeHTML;
+        resumeDisplayElement.style.display = 'block';
     }
     else {
         console.error("The resume display element is missing");
     }
-
-})
+  }
+});
